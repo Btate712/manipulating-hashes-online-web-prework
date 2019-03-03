ae09b8704a1c57cfe72e74912bf3c58a51181ff5
Robert Tate <btate712@gmail.com>
2019-03-03 13:10:05 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,17 @@ def first_challenge
   }
 
   #your code here
-
+contacts.each do |name, info|
+  info.each do |key, data|
+    if key == :favorite_icecream_flavors
+      data.each do |flavor|
+        if flavor == "strawberry"
+          favorite_icecream_flavors.delete("strawberry")
+        end
+      end
+    end
+  end
+end 
 
   #remember to return your newly altered contacts hash!
   contacts
